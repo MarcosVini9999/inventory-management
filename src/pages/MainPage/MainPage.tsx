c34da89f11ecde7c9e270ce84523294b22b6e44c
MarcosVini9999 <marcosviniciusandradedesousa@hotmail.com>
2022-10-28 19:30:57 -0300

    fix: infinite api requests

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -7,15 +7,18 @@ import React from "react";
 export const MainPage: React.FC = () => {
   const { postNewProductCardOnCart } = React.useContext(ProductCardContext);
   const [list, setList] = React.useState([]);
-  const fetchData = async () => {
-    try {
-      const result = await fakestoreApi.get(`/products/`);
-      setList(result.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-  fetchData();
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await fakestoreApi.get(`/products/`);
+        setList(result.data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <PageContainer>
       <ProductCardGenerator
